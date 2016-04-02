b866bca182126ee7faedea5ab97ebca45784afc1
Jakub Dubec <jakub.dubec@gmail.com>
2016-04-02 20:51:46 +0200

    From entities Gemlock

---
+++ src/TravelDiary/CoreBundle/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TravelDiary\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('usrFisrtname', TextType::class)
+            ->add('usrLastname', TextType::class)
+            ->add('usrEmail', EmailType::class)
+            ->add('usrPassword', RepeatedType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TravelDiary\CoreBundle\Entity\User'
+        ));
+    }
+}
