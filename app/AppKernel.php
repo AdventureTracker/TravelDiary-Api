3d74f1d743a242686bf57e5987265d561686a791
Jakub Dubec <jakub.dubec@gmail.com>
2016-05-11 09:53:19 +0200

    VichUploaderBundle installation & configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
             new TravelDiary\ApiBundle\TravelDiaryApiBundle(),
             new TravelDiary\CoreBundle\TravelDiaryCoreBundle(),
             new TravelDiary\InterfaceBundle\TravelDiaryInterfaceBundle(),
